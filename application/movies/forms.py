9ef8cb45770a193dff8758b855a7a027afbf563d
veliblesku <vili.stick@gmail.com>
2020-04-30 20:27:23 +0300

    heroku fix

--- application/movies/forms.py
+++ application/movies/forms.py
@@ -5,9 +5,9 @@ from application.roles.models import Role
 from application.movies.models import Movie
 
 class MovieForm(FlaskForm):
-    name = StringField("Movie name", [validators.Length(min=2)])
-    duration = IntegerField("Movie duration")
-    budget = FloatField("Movie budget in millions", [validators.NumberRange(min=0, max=9999999)])
+    name = StringField("Movie name", [validators.Length(min=2, max=144)])
+    duration = IntegerField("Movie duration", [validators.NumberRange(min=0, max=20000)])
+    budget = FloatField("Movie budget in millions", [validators.NumberRange(min=0, max=99999999)])
     year = IntegerField("Movie year", [validators.NumberRange(min=1500, max=2030)])
 
     
