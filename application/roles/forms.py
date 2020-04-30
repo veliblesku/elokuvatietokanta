7d0ec859204f4e0f8246c416f8505fd765cb51e5
veliblesku <vili.stick@gmail.com>
2020-04-30 19:49:23 +0300

    heroku

--- application/roles/forms.py
+++ application/roles/forms.py
@@ -5,14 +5,14 @@ from application.persons.models import Person
 from application.roles.models import Role
 
 class RoleForm(FlaskForm):
-    roleName = StringField("Role in movie", [validators.Length(min=2)])
+    rolename = StringField("Role in movie", [validators.Length(min=2)])
 
     
     class Meta:
         csrf = False
 
 class RoleFormAddPersonInMovie(FlaskForm):
-    roles = QuerySelectField(u'Role', query_factory=Role.get_role_list, get_label='roleName')
+    roles = QuerySelectField(u'Role', query_factory=Role.get_role_list, get_label='rolename')
     persons = QuerySelectField(u'Person', query_factory=Person.get_person_list, get_label='name')
     
 
--- application/roles/models.py
+++ application/roles/models.py
@@ -6,15 +6,15 @@ from application.models import Base
 
 class Role(Base):
 
-    roleName = db.Column(db.String(144), nullable=False)
+    rolename = db.Column(db.String(144), nullable=False)
 
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
     ## roles
     roles_in_movies = db.relationship("PersonsRoleInMovie", back_populates="role")
 
 
-    def __init__(self, roleName):
-        self.roleName = roleName
+    def __init__(self, rolename):
+        self.rolename = rolename
 
     def get_role_list():
         return Role.query
